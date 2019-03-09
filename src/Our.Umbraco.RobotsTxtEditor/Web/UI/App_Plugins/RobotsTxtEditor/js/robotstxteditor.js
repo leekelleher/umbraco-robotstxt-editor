dcaacb0cb1045d4268700eed4c24dd19aaf903c0
Emma Burstow <emma. burstow@poweredbypie.co.uk>
2019-03-09 14:29:26 +0000

    Angular controller passing string to the dashboard, button and text area in.

--- src/Our.Umbraco.RobotsTxtEditor/Web/UI/App_Plugins/RobotsTxtEditor/js/robotstxteditor.js
+++
@@ -1,5 +0,0 @@
-angular.module("umbraco")
-	.controller("RobotsTxtEditorController",
-	function () {
-		console.log("The robots.txt controller has landed");
-	});
---
+++ src/Our.Umbraco.RobotsTxtEditor/Web/UI/App_Plugins/RobotsTxtEditor/js/robotstxteditorController.js
@@ -0,0 +1,10 @@
+
+angular.module("umbraco")
+    .controller("RobotsTxtEditorController",
+        function ($http) {
+
+            var vm = this;
+            //vm.text = $http.get("backoffice/RobotsTxtEditor/RobotsTxtEditorApi/GetRobotsText");
+            vm.text = "A jubilatory Emma and Lotte";
+            console.log(vm.text);
+        });
