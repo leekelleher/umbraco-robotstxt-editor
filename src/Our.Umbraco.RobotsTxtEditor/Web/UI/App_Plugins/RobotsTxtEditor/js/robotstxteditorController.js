0d45ea90f30eb333f13c529c1799afc91c4ebc13
Lotte Pitcher <github@lottepitcher.co.uk>
2019-03-10 00:19:16 +0000

    reload button if file exists, and button says Create if missing, or Update if existing

--- src/Our.Umbraco.RobotsTxtEditor/Web/UI/App_Plugins/RobotsTxtEditor/js/robotstxteditorController.js
+++ src/Our.Umbraco.RobotsTxtEditor/Web/UI/App_Plugins/RobotsTxtEditor/js/robotstxteditorController.js
@@ -6,6 +6,7 @@ angular.module("umbraco")
 
         vm.getData = getData;
         vm.saveData = saveData;
+        vm.reloadData = reloadData;
 
         vm.loading = true;
         vm.data = null;
@@ -21,7 +22,15 @@ angular.module("umbraco")
             robotsTxtEditorResource.save(vm.data).then(function (response) {
                 var success = response.data;
                 console.log("API post returned " + success);
+                if (success) {
+                    vm.data.FileExists = true;
+                }
             });
+        }
+
+        function reloadData() {
+            vm.loading = false;
+            getData();
         }
 
         vm.getData();
