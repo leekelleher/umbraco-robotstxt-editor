c159069e383eeed91a663bbbf3081788ef8459b8
Mike <mike.masey@yoyodesign.com>
2019-03-28 21:05:20 +0000

    fix: unfinished merge and consitency of quotation marks

--- src/Our.Umbraco.RobotsTxtEditor/Web/UI/App_Plugins/RobotsTxtEditor/js/robotstxteditorController.js
+++ src/Our.Umbraco.RobotsTxtEditor/Web/UI/App_Plugins/RobotsTxtEditor/js/robotstxteditorController.js
@@ -12,21 +12,9 @@ angular.module("umbraco")
             vm.data = null;
 
 
-<<<<<<< HEAD
             function getData() {
                 robotsTxtEditorResource.get().then(function (response) {
                     vm.data = response.data;
-=======
-                if (vm.data.FileExists === false) {
-                    // TODO: See if there is a nice way of doing this whilst maintaining correct format
-                    var defaultValue = [];
-                    defaultValue.push("# default robots.txt content for Umbraco v8");
-                    defaultValue.push("\n\n");
-                    defaultValue.push("User-Agent: *\n");
-                    defaultValue.push("Disallow: /bin/\n");
-                    defaultValue.push("Disallow: /config/\n");
-                    defaultValue.push("Disallow: /umbraco/\n");
->>>>>>> origin/umb8
 
                     if (vm.data.FileExists === false) {
                         // TODO: See if there is a nice way of doing this whilst maintaining correct format
@@ -55,14 +43,13 @@ angular.module("umbraco")
                     console.log("API post returned " + success);
                     if (success) {
                         vm.data.FileExists = true;
-                        notificationsService.success('Saved', 'Text saved to Robots.txt');
+                        notificationsService.success("Saved", "Text saved to Robots.txt");
                     }
                 });
             }
 
             function reloadData() {
                 vm.loading = false;
-<<<<<<< HEAD
                 getData();
             }
 
@@ -72,7 +59,7 @@ angular.module("umbraco")
                     theme: "chrome",
                     showPrintMargin: false,
                     advanced: {
-                        fontSize: '14px',
+                        fontSize: "14px",
                         enableSnippets: true,
                         enableBasicAutocompletion: true,
                         enableLiveAutocompletion: false
@@ -91,8 +78,8 @@ angular.module("umbraco")
                             //Disable (alt+shift+K)
                             //Conflicts with our own show shortcuts dialog - this overrides it
                             {
-                                name: 'unSelectOrFindPrevious',
-                                bindKey: 'Alt-Shift-K',
+                                name: "unSelectOrFindPrevious",
+                                bindKey: "Alt-Shift-K",
                                 exec: function () {
                                     //Toggle the show keyboard shortcuts overlay
                                     $scope.$apply(function () {
@@ -119,18 +106,6 @@ angular.module("umbraco")
 
                 function changeAceEditor() {
                     setFormState("dirty");
-=======
-            });
-        }
-
-        function saveData() {
-            robotsTxtEditorResource.save(vm.data).then(function (response) {
-                var success = response.data;
-                console.log("API post returned " + success);
-                if (success) {
-                    vm.data.FileExists = true;
-                    notificationsService.success("Saved", "Text saved to Robots.txt");
->>>>>>> origin/umb8
                 }
 
                 function setFormState(state) {
